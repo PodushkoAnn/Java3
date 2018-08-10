9998013dde54dbc5d9ae0a89dee5a6b552c538d6
Ann <eni86@mail.ru>
2018-08-10 16:06:24 +0300

    домашняя работа к уроку 1

---
+++ bigTask/Apple.java
@@ -0,0 +1,15 @@
+package lesson1.homework.bigTask;
+
+public class Apple extends Fruit {
+    float weight = 1.0f;
+    String name = "Яблоко";
+
+    public float getWeight(){
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ bigTask/Orange.java
@@ -0,0 +1,14 @@
+package lesson1.homework.bigTask;
+
+public class Orange extends Fruit {
+    float weight = 1.5f;
+    String name = "Апельсин";
+
+    public float getWeight(){
+        return weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
